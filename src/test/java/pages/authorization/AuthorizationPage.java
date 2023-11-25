d19b71f55f5f364a96a9435e1462f812799b8429
Хныкин Роман <rkhnykin@rvision.ru>
2023-11-25 16:29:46 +0300

    Убрал лишнее

--- src/test/java/pages/authorization/AuthorizationPage.java
+++ src/test/java/pages/authorization/AuthorizationPage.java
@@ -1,14 +1,6 @@
 package pages.authorization;
 
-import com.codeborne.selenide.SelenideElement;
-import org.openqa.selenium.By;
-
 import static com.codeborne.selenide.Selenide.element;
 
 public class AuthorizationPage {
-    private final By TITLE = By.xpath("//h1");
-
-    public SelenideElement getTitle() {
-        return element(TITLE);
-    }
 }
