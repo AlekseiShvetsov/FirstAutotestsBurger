6bfc49c4952fbe83731ebda36dd8146944e364f1
Aleksei Shvetsov <alex20-03sh@mail.ru>
2023-11-26 19:23:01 +0100

    Дописал строчку проверки авторизации.

--- src/test/java/test/authorization/Authorization.java
+++ src/test/java/test/authorization/Authorization.java
@@ -2,7 +2,13 @@ package test.authorization;
 
 import org.testng.annotations.Test;
 import pages.authorization.AuthorizationPage;
+import pages.constructor.ConstructorPage;
+import pages.profile.ProfilePage;
 import services.authorization.AuthorizationService;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.element;
 
 public class Authorization {
     private final AuthorizationService service = new AuthorizationService();
@@ -10,11 +16,14 @@ public class Authorization {
     private final AuthorizationPage password = new AuthorizationPage();
     private final AuthorizationPage enter = new AuthorizationPage();
 
+    private final ConstructorPage page = new ConstructorPage();
+
     @Test
     public void checkAuthorization() {
         service.openAutorizationPage();
         email.email();
         password.password();
         enter.enterButton();
+        element(page.getTitle()).shouldBe(visible.because("Пользователь не авторизован"));
     }
 }
