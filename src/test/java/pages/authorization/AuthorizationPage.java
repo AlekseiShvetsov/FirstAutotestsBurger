8e3c34f5a38079b28679284fdb6fc04996af1f3a
Aleksei Shvetsov <alex20-03sh@mail.ru>
2023-11-28 02:55:58 +0100

    подредактировал импорты

--- src/test/java/pages/authorization/AuthorizationPage.java
+++ src/test/java/pages/authorization/AuthorizationPage.java
@@ -3,8 +3,7 @@ package pages.authorization;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Selectors.*;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.element;
 
 
 public class AuthorizationPage {
